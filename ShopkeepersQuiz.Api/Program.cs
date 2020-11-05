b4a1f2ff67fa329ea2b9948ae2234aef180b4ed3
Sam Williams <swilliams96@outlook.com>
2020-11-05 23:05:08 +0000

    Add logging with Serilog

--- ShopkeepersQuiz.Api/Program.cs
+++ ShopkeepersQuiz.Api/Program.cs
@@ -1,25 +1,57 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using ShopkeepersQuiz.Api.BackgroundServices;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace ShopkeepersQuiz.Api
 {
 	public class Program
 	{
+		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production" }.json", optional: true)
+			.AddEnvironmentVariables("ShopkeepersQuizApi_")
+			.Build();
+
+		public static readonly string ApplicationName = Configuration.GetValue("Name", "Shopkeeper's Quiz API");
+
+		public static readonly string ApplicationVersion = Configuration.GetValue("Version", "0.0.0");
+
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(Configuration)
+				.Enrich.FromLogContext()
+				.Enrich.WithProperty(nameof(ApplicationVersion), ApplicationVersion)
+				.CreateLogger();
+
+			try
+			{
+				Log.Information($"Starting {ApplicationName} v{ApplicationVersion}...");
+
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Program terminated unexpectedly!");
+
+				throw;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseSerilog()
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
